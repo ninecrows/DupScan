572f901b26951657e9962ff67288a968b7ba5ade
Kyle Wilson <kyle@ninecrows.com>
2019-04-25 17:01:21 -0400

    Adding bits to make a display information tool

--- ShowDisplays/GetDisplays.cs
+++ ShowDisplays/GetDisplays.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,168 @@ namespace ShowDisplays
 {
     class GetDisplays
     {
+        public DisplayInfoCollection displays { get; } 
+
+        public GetDisplays()
+        {
+            displays = GetDisplaysHere();
+        }
+
+        delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref Rect lprcMonitor, IntPtr dwData);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip,
+    /*EnumMonitorsDelegate*/ MonitorEnumDelegate lpfnEnum, IntPtr dwData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        /// <summary>
+        /// The struct that contains the display information
+        /// </summary>
+        public class DisplayInfo
+        {
+            public string Availability { get; set; }
+            public string ScreenHeight { get; set; }
+            public string ScreenWidth { get; set; }
+            public Rect MonitorArea { get; set; }
+            public Rect WorkArea { get; set; }
+            public string Path { get; set; }
+            public uint Flags { get; set; }
+        }
+        /// <summary>
+        /// Collection of display information
+        /// </summary>
+        public class DisplayInfoCollection : List<DisplayInfo>
+        {
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MonitorInfoEx lpmi);
+
+        /// <summary>
+        /// Returns the number of Displays using the Win32 functions
+        /// </summary>
+        /// <returns>collection of Display Info</returns>
+        public static DisplayInfoCollection GetDisplaysHere()
+        {
+            DisplayInfoCollection col = new DisplayInfoCollection();
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
+                delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref Rect lprcMonitor, IntPtr dwData)
+                {
+                    MonitorInfoEx mi = new MonitorInfoEx();
+                    mi.Size = (int)Marshal.SizeOf(mi);
+                    bool success = GetMonitorInfo(hMonitor, ref mi);
+                    if (success)
+                    {
+                        DisplayInfo di = new DisplayInfo();
+                        di.ScreenWidth = (mi.Monitor.right - mi.Monitor.left).ToString();
+                        di.ScreenHeight = (mi.Monitor.bottom - mi.Monitor.top).ToString();
+                        di.MonitorArea = mi.Monitor;
+                        di.WorkArea = mi.Work;
+                        di.Availability = mi.Flags.ToString();
+                        di.Path = mi.DeviceName;
+                        di.Flags = mi.Flags;
+
+                        col.Add(di);
+                    }
+                    return true;
+                }, IntPtr.Zero);
+            return col;
+        }
+
+        // size of a device name string
+        private const int CCHDEVICENAME = 32;
+
+        /// <summary>
+        /// The MONITORINFOEX structure contains information about a display monitor.
+        /// The GetMonitorInfo function stores information into a MONITORINFOEX structure or a MONITORINFO structure.
+        /// The MONITORINFOEX structure is a superset of the MONITORINFO structure. The MONITORINFOEX structure adds a string member to contain a name 
+        /// for the display monitor.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        internal struct MonitorInfoEx
+        {
+            /// <summary>
+            /// The size, in bytes, of the structure. Set this member to sizeof(MONITORINFOEX) (72) before calling the GetMonitorInfo function. 
+            /// Doing so lets the function determine the type of structure you are passing to it.
+            /// </summary>
+            public int Size;
+
+            /// <summary>
+            /// A RECT structure that specifies the display monitor rectangle, expressed in virtual-screen coordinates. 
+            /// Note that if the monitor is not the primary display monitor, some of the rectangle's coordinates may be negative values.
+            /// </summary>
+            public Rect Monitor;
+
+            /// <summary>
+            /// A RECT structure that specifies the work area rectangle of the display monitor that can be used by applications, 
+            /// expressed in virtual-screen coordinates. Windows uses this rectangle to maximize an application on the monitor. 
+            /// The rest of the area in rcMonitor contains system windows such as the task bar and side bars. 
+            /// Note that if the monitor is not the primary display monitor, some of the rectangle's coordinates may be negative values.
+            /// </summary>
+            public Rect Work;
+
+            /// <summary>
+            /// The attributes of the display monitor.
+            /// 
+            /// This member can be the following value:
+            ///   1 : MONITORINFOF_PRIMARY
+            /// </summary>
+            public uint Flags;
+
+            /// <summary>
+            /// A string that specifies the device name of the monitor being used. Most applications have no use for a display monitor name, 
+            /// and so can save some bytes by using a MONITORINFO structure.
+            /// </summary>
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string DeviceName;
+
+            public void Init()
+            {
+                this.Size = 40 + 2 * CCHDEVICENAME;
+                this.DeviceName = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// The RECT structure defines the coordinates of the upper-left and lower-right corners of a rectangle.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/dd162897%28VS.85%29.aspx"/>
+        /// <remarks>
+        /// By convention, the right and bottom edges of the rectangle are normally considered exclusive. 
+        /// In other words, the pixel whose coordinates are ( right, bottom ) lies immediately outside of the the rectangle. 
+        /// For example, when RECT is passed to the FillRect function, the rectangle is filled up to, but not including, 
+        /// the right column and bottom row of pixels. This structure is identical to the RECTL structure.
+        /// </remarks>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RectStruct
+        {
+            /// <summary>
+            /// The x-coordinate of the upper-left corner of the rectangle.
+            /// </summary>
+            public int Left;
+
+            /// <summary>
+            /// The y-coordinate of the upper-left corner of the rectangle.
+            /// </summary>
+            public int Top;
+
+            /// <summary>
+            /// The x-coordinate of the lower-right corner of the rectangle.
+            /// </summary>
+            public int Right;
+
+            /// <summary>
+            /// The y-coordinate of the lower-right corner of the rectangle.
+            /// </summary>
+            public int Bottom;
+        }
+
     }
 }
--- ShowDisplays/MainWindow.xaml.cs
+++ ShowDisplays/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace ShowDisplays
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Get the list of displays connected to this system.
+        GetDisplays displays = new GetDisplays();
+
         public MainWindow()
-        {
+        {        
             InitializeComponent();
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            Grid myGrid = new Grid();
+            myGrid.Width = 250;
+            myGrid.Height = 100;
+            MainGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            MainGrid.VerticalAlignment = VerticalAlignment.Top;
+            MainGrid.ShowGridLines = true;
+
+            RowDefinition rowDef1 = new RowDefinition();
+            RowDefinition rowDef2 = new RowDefinition();
+            RowDefinition rowDef3 = new RowDefinition();
+            RowDefinition rowDef4 = new RowDefinition();
+            MainGrid.RowDefinitions.Add(rowDef1);
+            MainGrid.RowDefinitions.Add(rowDef2);
+            MainGrid.RowDefinitions.Add(rowDef3);
+            MainGrid.RowDefinitions.Add(rowDef4);
+
+            int where = 0;
+            foreach (var item in displays.displays)
+            {
+                var control = new Button();
+                control.Content = item.Path;
+                //MainGrid.Children.Add(control);
+                
+
+                Grid.SetRow(control, where);
+            }
         }
     }
 }
