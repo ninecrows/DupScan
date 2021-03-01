5744145692651a20ea974c9d6386627901bb46d4
Kyle Wilson <kyle@ninecrows.com>
2021-03-01 00:12:26 -0500

    Transition to name and core for helpers

---
+++ C9FileHelpers/ExTimeStamp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9FileHelpers
+{
+    [Serializable]
+    public class ExTimeStamp
+    {
+        public ExTimeStamp(DateTime when)
+        {
+            Time = when;
+            Iso8601 = when.ToUniversalTime().ToString("yyyyMMddThhMMss.fffZ");
+            Raw = when.Ticks;
+        }
+
+        public DateTime Time
+        {
+            get;
+        }
+
+        public string Iso8601
+        {
+            get;
+        }
+
+        public long Raw
+        {
+            get;
+        }
+    }
+}
