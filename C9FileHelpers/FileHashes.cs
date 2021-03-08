20a62197cd0d0747f16ea10729bb29669538daa5
Kyle Wilson <kyle@ninecrows.com>
2021-03-08 09:03:29 -0500

    Minor update

--- C9FileHelpers/FileHashes.cs
+++ C9FileHelpers/FileHashes.cs
@@ -5,7 +5,9 @@ using System.Reflection;
 using System.Runtime.Serialization;
 using System.Security.Cryptography;
 using System.Text;
+using Force.Crc32;
 using MongoDB.Bson.Serialization.IdGenerators;
+//using Crc32CAlgorithm = Crc32C.Crc32CAlgorithm;
 
 namespace C9FileHelpers
 {
@@ -31,17 +33,15 @@ namespace C9FileHelpers
 
         public FileHashes(string fileName)
         {
-            makeHashes(fileName);
+            MakeHashes(fileName);
         }
 
         /// <summary>
         /// Calculate hashes by reading the file and spinning threads to calculate hashes.
         /// </summary>
         /// <param name="fileName"></param>
-        public void makeHashes(string fileName)
+        public void MakeHashes(string fileName)
         {
-            //byte[] buffer;
-
             var infile = File.OpenRead(fileName);
 
             byte[] buffer = File.ReadAllBytes(fileName);
@@ -53,77 +53,131 @@ namespace C9FileHelpers
                 string valueBase64 = Convert.ToBase64String(value);
 
                 fileHashSha256 = valueBase64;
-
-                var segments = new BufferSegments(buffer, 64);
             }
-        }
 
-#if false
-        public string BytesToHex(byte[] buffer)
-        {
-            StringBuilder working = new StringBuilder();
-
-            foreach (var item in buffer)
+            using (SHA512 mySHA512 = SHA512.Create())
             {
-                working.AppendFormat("{0:X2} ", item);
+                byte[] value = mySHA512.ComputeHash(buffer);
+
+                string valueBase64 = Convert.ToBase64String(value);
+
+                fileHashSha512 = valueBase64;
             }
 
-            return working.ToString();
+            using (SHA1 mySHA1 = SHA1.Create())
+            {
+                byte[] value = mySHA1.ComputeHash(buffer);
+
+                string valueBase64 = Convert.ToBase64String(value);
+
+                fileHashSha1 = valueBase64;
+            }
+
+            using (MD5 myMD5 = MD5.Create())
+            {
+                byte[] value = myMD5.ComputeHash(buffer);
+
+                string valueBase64 = Convert.ToBase64String(value);
+
+                fileHashMd5 = valueBase64;
+            }
+
+            var result = Crc32CAlgorithm.Compute(buffer);
+
+            var trad = Crc32Algorithm.Compute(buffer);
+
+            var segments = new BufferSegments(buffer, 64);
         }
-#endif
     }
 
-    class BufferSegments
+    /// <summary>
+    /// Given a buffer and a size for lead and trail bits, extract what we can and provide it in various forms.
+    /// </summary>
+    internal class BufferSegments
     {
-        private byte[] leadBytes;
-        private byte[] trailBytes;
+        /// <summary>
+        /// Retrieve raw binary lead byte data.
+        /// </summary>
+        public byte[] LeadBytes { get; }
 
-        private string leadData;
-        public string LeadData
-        {
-            get => leadData;
-        }
-        private string trailData;
-        public string TrailData
-        {
-            get => trailData; 
-        }
+        /// <summary>
+        /// Retrieve raw binary trail byte data.
+        /// </summary>
+        public byte[] TrailBytes { get; }
 
+        /// <summary>
+        /// Hex encoded lead byte data.
+        /// </summary>
+        public string LeadData { get; }
+
+        /// <summary>
+        /// Hex encoded trail byte data.
+        /// </summary>
+        public string TrailData { get; }
+
+        /// <summary>
+        /// Leading section of the buffer as text.
+        /// </summary>
+        public string LeadText { get; }
+
+        /// <summary>
+        /// Retrieve the binary buffer data as base 64 encoded string.
+        /// </summary>
+        public string LeadBase64 { get; }
+
+        /// <summary>
+        /// Retrieve the binary buffer data as base64 encoded string.
+        /// </summary>
+        public string TrailBase64 { get; }
+
+        /// <summary>
+        /// Given a large buffer full of file data and a length, load this object with appropriate bits.
+        /// </summary>
+        /// <param name="buffer">Buffer containing all of the data in the file.</param>
+        /// <param name="length">Number of lead and trail bytes we'd like to keep.</param>
         public BufferSegments(byte[] buffer, int length)
         {
             // Plenty of data so we get full sized lead and trail segments.
             if (buffer.Length >= length)
             {
-                leadBytes = new byte[length];
+                LeadBytes = new byte[length];
                 for (int index = 0; index < length; index++)
                 {
-                    leadBytes[index] = buffer[index];
+                    LeadBytes[index] = buffer[index];
                 }
 
-                trailBytes = new byte[length];
+                TrailBytes = new byte[length];
                 for (int index = 0; index < length; index++)
                 {
-                    trailBytes[index] = buffer[buffer.Length + index - length];
+                    TrailBytes[index] = buffer[buffer.Length + index - length];
                 }
             }
 
             // Short buffer so lead and trail are the same data.
             else
             {
-                leadBytes = buffer;
-                trailBytes = buffer;
+                LeadBytes = buffer;
+                TrailBytes = buffer;
             }
 
             // Now calculate the hex encoded versions
-            leadData = BytesToHex(leadBytes);
-            trailData = BytesToHex(trailBytes);
+            LeadData = BytesToHex(LeadBytes);
+            TrailData = BytesToHex(TrailBytes);
 
-            string tttaaa = Encoding.ASCII.GetString(leadBytes);
+            // Grab the lead area as text characters (mostly aimed at PDF files).
+            LeadText = Encoding.ASCII.GetString(LeadBytes);
 
+            LeadBase64 = Convert.ToBase64String(LeadBytes);
+            TrailBase64 = Convert.ToBase64String(TrailBytes);
+        }
+
+        // ReSharper disable once IdentifierTypo
+        // ReSharper disable once UnusedMember.Local
+        private static string AsciifyBuffer(IEnumerable<byte> buffer)
+        {
             StringBuilder builder = new StringBuilder();
-            for (int index = 0; index < trailBytes.Length; index++)
+            foreach (var current in buffer)
             {
-                byte current = leadBytes[index];
                 if ((current >= 32 && current < 0x7f) ||
                     current == 10 || current == 13 || current == 9)
                 {
@@ -135,10 +189,10 @@ namespace C9FileHelpers
                 }
             }
 
-            string readable = builder.ToString();
+            return builder.ToString();
         }
 
-        private string BytesToHex(byte[] buffer)
+        private static string BytesToHex(byte[] buffer)
         {
             StringBuilder working = new StringBuilder();
 
